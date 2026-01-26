491a8279295de4d30adfdabf1c0ac55388439a5f
barjun25 <97790264+barjun25@users.noreply.github.com>
2026-01-26 20:07:23 +0530

    rebuild changes
    rebuild changes

--- dist/my-profile/server/angular-app-manifest.mjs
+++ dist/my-profile/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/MyAngProfile/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/MyAngProfile"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 846, hash: '5ddb1ea83ec0f358d6740292bd193ec00af11994892bdcb0c3930ee9d3146faa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1052, hash: '4cfcad183be3a88e07d26fa6d7ae622810b5f5a44dd13cfc2ba34ed8179924fb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 13974, hash: 'f0bace880241d90ed43ace6de6c745d3353656c58c293f22502e20da94252c08', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 859, hash: '01f05d882031e204bcf327d5e96ead64291a2460272b4744a2fcdd7c746d4282', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1065, hash: '13893ddb7025d9b935690d2aebe40032b9492a00958315c56461358f2ff8e098', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 13987, hash: '5f232bc7788df7f17e3973efa8498eb4f354470cbf26e46125c2a08911f8ee49', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-AHGEH5LC.css': {size: 182, hash: 'lKzg+elY7pc', text: () => import('./assets-chunks/styles-AHGEH5LC_css.mjs').then(m => m.default)}
   },
 };
