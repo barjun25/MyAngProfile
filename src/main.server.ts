3caed3abe445ca3bc2ab4ed99247605fb4628d01
barjun25 <97790264+barjun25@users.noreply.github.com>
2026-01-27 09:29:43 +0530

    Delete src directory

--- src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { FooterComponent } from './app/footer/footer.component';
-
-const bootstrap = () => bootstrapApplication(AppComponent);
-
-export default bootstrap;
