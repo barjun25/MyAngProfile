52e4e5b547661633155655a7ea3a14011c815d66
barjun25 <barjun25@gmail.com>
2026-02-02 11:15:32 +0530

    New gemini changes commit

--- dist/my-profile/server/angular-app-engine-manifest.mjs
+++ dist/my-profile/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/MyAngProfile',
   supportedLocales: {
   "en-US": ""
 },
--- dist/my-profile/server/angular-app-manifest.mjs
+++ dist/my-profile/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/MyAngProfile/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/MyAngProfile"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 961, hash: '53824a4f5c46a742a36d6b441006b40521be24036a1121847b4b2bb9ee75ff97', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1052, hash: '5e2efb163d1aa7cc34b4fc2b332f469cf0b007bd4107b32b655500442a363b0f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 17249, hash: '99198e75fea2e907b2dfa5a512ed929dc7b5a5a16f0bd3be4bb0fdd45462764d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-H7U2XHGY.css': {size: 342, hash: 'gYPJNkxBpU0', text: () => import('./assets-chunks/styles-H7U2XHGY_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 1031, hash: '8433b2418b48c71547dd78858f2193f3647bdbd3be117e88480761e66202d2e0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1126, hash: '33b9d6c90d1c09cf87059e3eb9b41356af96cd3a9fd00017163078ba69aa8c37', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 18367, hash: '0eab53592bbac256c22a08c2f2fa56924d8e5a0a6f3ad8722b73ac2b35984845', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-RQLLAF7M.css': {size: 1243, hash: 'yoPfRuexuCM', text: () => import('./assets-chunks/styles-RQLLAF7M_css.mjs').then(m => m.default)}
   },
 };
